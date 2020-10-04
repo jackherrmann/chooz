6360c18596fd9c62acaac31517d80430a5bc0943
alexandrashaw10 <alels4321@gmail.com>
2020-10-03 20:24:21 -0400

    add location access and change start session to be a class

--- src/components/StartSession.js
+++ src/components/StartSession.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 // import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
 import Card from '@material-ui/core/Card';
@@ -13,13 +13,10 @@ import Select from '@material-ui/core/Select';
 // import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid'; 
 import Slider from '@material-ui/core/Slider'; 
 
 
-
-
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
     container: {
         width: '100%', 
         display: 'flex', 
@@ -72,171 +69,197 @@ const useStyles = makeStyles((theme) => ({
         marginLeft: "0.5rem", 
         width: '17%', 
     }
-}));
+  });
 
 
 function getMenuItems(arr) {
     const items = []; 
     var item; 
     for (item in arr) {
-        items.push(<MenuItem value={arr[item]}> {arr[item]} </MenuItem>); 
+        items.push(<MenuItem key={item} value={arr[item]}> {arr[item]} </MenuItem>); 
     }
     return items; 
 }
 
-export default function StartSession(props) {
-
-    const socket = props.socket;
-
-    const classes = useStyles(); 
-
-    const [activityType, setActivityType] = React.useState(''); 
-    const [activityGenres, setActivityGenre] = React.useState([]); 
-    const [activityPrice, setActivityPrice] = React.useState(''); 
-    const [numItems, setNumItems] = React.useState(10); 
-
-    var keywordInput = ""; 
-    var nameInput = ""; 
-    const handleNamechange = (event) => {
-        nameInput = event.target.value; 
-    }
-    const handleActivityChange = (event) => {
-        setActivityType(event.target.value);
-        setActivityGenre([]); 
-    }
-    const handleGenresChange = (event) => {
-        setActivityGenre(event.target.value); 
-    }
-    const handlePriceChange = (event) => {
-        setActivityPrice(event.target.value); 
-    }
-    const handleKeywordChange = (event) => {
-        keywordInput = event.target.value; 
-    }
-    const handleNumItemsInputChange = (event) => {
-        setNumItems(event.target.value); 
-    }
-    const handleNumItemsSliderChange = (event, newValue) => {
-        setNumItems(newValue); 
-    }
-    const handleCreate = () => {
-        const data = {
-            'name': nameInput,
-            'activityType': activityType,
-            'numSwipes': numItems
+class StartSession extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          count: 0,
+          activityType: '',
+          activityGenres: [], 
+          activityPrice: '',
+          numItems: 10,
+          latitude: 0,
+          longitude: 0,
         };
-        socket.emit('create_session', data);
     }
 
-    const handleBlur = () => {
-        console.log("handled blur with num: " + numItems); 
-        if (numItems < 1) {
-          setNumItems(1);
-        } else if (numItems > 100) {
-          setNumItems(100);
+    componentDidMount() {
+        const setLocationState = (lat, long) => {
+            this.setState({ latitude: lat });
+            this.setState({ longitude: long });
         }
-      };
 
-    const genres = {
-        "Food": ["Any", "Chinese", "Pizza", "Other"], 
-        "Movies": ["Any", "Comedy", "Action", "Horror"], 
-        "Events": ["Any", "Concert", "Protest", "Other"]
-    }; 
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                console.log('latitude: ' + position.coords.latitude);
+                console.log('longitude' + position.coords.longitude);
+                setLocationState(position.coords.latitude, position.coords.longitude)
+            },
+            function(error) {
+                console.error("Error Code = " + error.code + " - " + error.message);
+            }
+        );
+    }
 
-    const prices = ["Any", "$", "$$", "$$$"]; 
 
-    return (
+    render () {
+        const socket = this.props.socket;
 
-        <Container className={classes.container}>
+        const { classes } = this.props; 
 
-            <Typography className={classes.title} color="primary" variant="h1"> chooz.io</Typography>
+        var keywordInput = ""; 
+        var nameInput = ""; 
+        const handleNamechange = (event) => {
+            nameInput = event.target.value; 
+        }
+        const handleActivityChange = (event) => {
+            this.setState({ activityType: event.target.value });
+            this.setState({ activityGenre: [] });
+        }
+        const handleGenresChange = (event) => {
+            this.setState({ activityGenre: event.target.value });
+        }
+        const handlePriceChange = (event) => {
+            this.setState({ activityPrice: event.target.value });
+        }
+        const handleKeywordChange = (event) => {
+            keywordInput = event.target.value; 
+        }
+        const handleNumItemsInputChange = (event) => {
+            this.setState({ numItems: event.target.value });
+        }
+        const handleNumItemsSliderChange = (event, newValue) => {
+            this.setState({ numItems: newValue });
+        }
+        const handleCreate = () => {
+            const data = {
+                'name': nameInput,
+                'activityType': this.state.activityType,
+                'numSwipes': this.state.numItems,
+                'latitude': this.state.latitude,
+                'longitude': this.state.longitude,
+            };
+            socket.emit('create_session', data);
+        }
 
-            <Card className={classes.card}>
+        const handleBlur = () => {
+            console.log("handled blur with num: " + this.state.numItems); 
+            if (this.state.numItems < 1) {
+                this.setState({ numItems: 1 });
+            } else if (this.state.numItems > 100) {
+                this.setState({ numItems: 100 });
+            }
+        };
 
-                <Typography className={classes.cardTitle} color="primary" variant="h4"> Create a Session</Typography>
+        const genres = {
+            "Food": ["Any", "Chinese", "Pizza", "Other"], 
+            "Movies": ["Any", "Comedy", "Action", "Horror"], 
+            "Events": ["Any", "Concert", "Protest", "Other"]
+        }; 
 
-                <TextField className={classes.keywordInput} id="name" label="Your Name" defaultValue="" onChange={handleNamechange} />
+        const prices = ["Any", "$", "$$", "$$$"]; 
 
-                <FormControl className={classes.formControl}>
-                    <InputLabel id="ActivityTypeLabel" className={classes.selectLabel}> Select Activity </InputLabel>
-                     <Select
-                        labelId="ActivityTypeLabel"
-                        id="ActivityType"
-                        value={activityType}
-                        onChange={handleActivityChange}
-                        className={classes.select}
-                    >
-                        {getMenuItems(Object.keys(genres))}
-                    </Select>
+        return (
 
-                </FormControl>
-                <FormControl className={classes.formControl}>
-                    <InputLabel id="GenreLabel" className={classes.selectLabel}> Select Genre </InputLabel>
-                    <Select
-                            labelId="ActivityGenreLabel"
-                            id="ActivityGenre"
-                            value={activityGenres}
-                            onChange={handleGenresChange}
+            <Container className={classes.container}>
+
+                <Typography className={classes.title} color="primary" variant="h1"> chooz.io</Typography>
+
+                <Card className={classes.card}>
+
+                    <Typography className={classes.cardTitle} color="primary" variant="h4"> Create a Session</Typography>
+
+                    <TextField className={classes.keywordInput} id="name" label="Your Name" defaultValue="" onChange={handleNamechange} />
+
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="ActivityTypeLabel" className={classes.selectLabel}> Select Activity </InputLabel>
+                        <Select
+                            labelId="ActivityTypeLabel"
+                            id="ActivityType"
+                            value={this.state.activityType}
+                            onChange={this.state.handleActivityChange}
                             className={classes.select}
-                            multiple
-                            input={<Input />}
-                            renderValue={(selected) => selected.join(', ')}
                         >
-                            {getMenuItems(genres[activityType])}
+                            {getMenuItems(Object.keys(genres))}
                         </Select>
-                </FormControl>
 
-                <FormControl className={classes.formControl}>
-                    <InputLabel id="PriceLabel" className={classes.selectLabel}> Select Price </InputLabel>
-                    <Select
-                        labelId="PriceLabel"
-                        id="ActivityPrice"
-                        value={activityPrice}
-                        onChange={handlePriceChange}
-                        className={classes.select}
-                    >
-                        {getMenuItems(prices)}
-                    </Select>
-                </FormControl>
+                    </FormControl>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="GenreLabel" className={classes.selectLabel}> Select Genre </InputLabel>
+                        <Select
+                                labelId="ActivityGenreLabel"
+                                id="ActivityGenre"
+                                value={this.state.activityGenres}
+                                onChange={handleGenresChange}
+                                className={classes.select}
+                                multiple
+                                input={<Input />}
+                                renderValue={(selected) => selected.join(', ')}
+                            >
+                                {getMenuItems(genres[this.state.activityType])}
+                            </Select>
+                    </FormControl>
 
-                <TextField className={classes.keywordInput} id="keyword" label="Enter Keyword (Optional)" defaultValue="" onChange={handleKeywordChange} />
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="PriceLabel" className={classes.selectLabel}> Select Price </InputLabel>
+                        <Select
+                            labelId="PriceLabel"
+                            id="ActivityPrice"
+                            value={this.state.activityPrice}
+                            onChange={handlePriceChange}
+                            className={classes.select}
+                        >
+                            {getMenuItems(prices)}
+                        </Select>
+                    </FormControl>
 
-                <InputLabel id="numItemsLabel" className={classes.sliderLabel}> Number of Items </InputLabel>
-                <div className={classes.sliderContainer}> 
-                    <Slider
-                            className={classes.slider}
-                            value={typeof numItems === 'number' ? numItems : 10}
-                            onChange={handleNumItemsSliderChange}
-                            aria-labelledby="numItemsLabel"
-                    />
-                    <Input
-                        className={classes.sliderNumber}
-                        value={numItems}
-                        margin="dense"
-                        onChange={handleNumItemsInputChange}
-                        onBlur={handleBlur}
-                        inputProps={{
-                        step: 1,
-                        min: 1,
-                        max: 100,
-                        type: 'number',
-                        'aria-labelledby': 'input-slider',
-                        }}
-                    />
-                </div>
+                    <TextField className={classes.keywordInput} id="keyword" label="Enter Keyword (Optional)" defaultValue="" onChange={handleKeywordChange} />
 
-                <Button className={classes.createButton} variant="contained" onClick={handleCreate}>Create Session</Button>
+                    <InputLabel id="numItemsLabel" className={classes.sliderLabel}> Number of Items </InputLabel>
+                    <div className={classes.sliderContainer}> 
+                        <Slider
+                                className={classes.slider}
+                                value={typeof this.state.numItems === 'number' ? this.state.numItems : 10}
+                                onChange={handleNumItemsSliderChange}
+                                aria-labelledby="numItemsLabel"
+                        />
+                        <Input
+                            className={classes.sliderNumber}
+                            value={this.state.numItems}
+                            margin="dense"
+                            onChange={handleNumItemsInputChange}
+                            onBlur={handleBlur}
+                            inputProps={{
+                            step: 1,
+                            min: 1,
+                            max: 100,
+                            type: 'number',
+                            'aria-labelledby': 'input-slider',
+                            }}
+                        />
+                    </div>
 
+                    <Button className={classes.createButton} variant="contained" onClick={handleCreate}>Create Session</Button>
 
-                
+                </Card>
 
+            </Container>
 
-
-            </Card>
-
-
-        </Container>
-
-    ); 
+        ); 
+    }
 
 }
+
+export default withStyles(styles, {withTheme: true})(StartSession);
