c6907dd88bf726337f7f3706a3a0ed00123855fd
xavlee <xavlee@sas.upenn.edu>
2020-10-04 00:58:29 -0400

    got rid of import start session

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
-const { default: StartSession } = require('../components/StartSession');
 
 const { Session } = require('./session');
 
